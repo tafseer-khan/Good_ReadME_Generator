b46817b9fd42ae3131e23e80cefd8259b2d280ac
tafseer-khan <tafseer.k.khan@gmail.com>
2021-01-24 19:11:46 -0600

    Added input questions

--- index.js
+++ index.js
@@ -4,6 +4,48 @@ const util = require('util')
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: "What is your title for your Project?",
+        name:'title'
+    },
+    {
+      type: 'input',
+      message: "What are your instructions for installation?",
+      name:'install'  
+    },
+    {
+        type: 'input',
+        messsage: 'What is your usage information?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What are your contribution guidlines?',
+        name: 'guidelines'
+    },
+    {
+        type: 'input',
+        message: 'What are your instructions for testing?',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: "Which liscense would you like to use?",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    }
+    
 
 ];
 
